80f92baf2f59eb0d3cb617db964cc138d1f51556
Abid Lohan <abidlohan@pop-os.localdomain>
2021-12-03 00:41:42 -0300

    Banana added

--- src/main/java/com/abidlohan/jobinmod/item/ModItems.java
+++ src/main/java/com/abidlohan/jobinmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.abidlohan.jobinmod.item;
 
 import com.abidlohan.jobinmod.JobinMod;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,6 +18,11 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JOBIN_TAB)));
     public static final RegistryObject<Item> RAW_JOBIN = ITEMS.register("raw_jobin",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JOBIN_TAB)));
+
+    public static final RegistryObject<Item> BANANA = ITEMS.register("banana",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JOBIN_TAB)
+                    .food(new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).build())
+            ));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
